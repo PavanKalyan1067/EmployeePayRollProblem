c6bce4e1402c8569733502d73a6d944be759cc4f
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-18 10:48:10 +0530

    Retrive all the data

---
+++ UC4.sql
@@ -0,0 +1,19 @@
+create database EmployeeServices;
+
+--UC-2 creating Table
+create Table empoyee_payrolls
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+--UC-3 Insert values in Table
+Insert into empoyee_payroll(name,salary,startDate) values
+('Pavan',20000,'2021-03-12'),
+('Kalyan',25000,'2021-04-18'),
+('Yadav',10000,'2020-05-13'),
+('Madhu',30000,'2020-08-19');
+
+--UC-4 Retrieve All data--
+select * from empoyee_payroll;
