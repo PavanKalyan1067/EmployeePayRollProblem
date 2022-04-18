ea9a5edb04dc9db4f75727484d527fff17d4bac6
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-18 10:50:20 +0530

    Ability to add gender

---
+++ UC6.sql
@@ -0,0 +1,35 @@
+create database EmployeeServices;
+
+--UC-2 creating Table
+create Table empoyee_payrolls
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+--UC-3 Insert values in Table
+Insert into empoyee_payroll(name,salary,startDate) values
+('Pavan',20000,'2021-03-12'),
+('Kalyan',25000,'2021-04-18'),
+('Yadav',10000,'2020-05-13'),
+('Madhu',30000,'2020-08-19');
+
+--UC-4 Retrieve All data--
+select * from empoyee_payroll;
+
+------- UC 5: Select Query using Cast() an GetDate() -------
+select salary from empoyee_payroll where name='Pavan';
+select salary from empoyee_payroll where startDate BETWEEN Cast('2020-12-20' as Date) and GetDate();
+
+------- UC 6: Add Gender Column and Update Table Values -------
+Alter table empoyee_payroll
+add Gender char(1);
+
+Update empoyee_payroll 
+set Gender='M'
+where name='Pavan';
+Update empoyee_payroll 
+set Gender='M'
+where name='Kalyan' or name='Yadav'or name='Madhu';
+
