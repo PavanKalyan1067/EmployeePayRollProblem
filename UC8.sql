30fc038e1487091778d3b55b44c7e7030e581a94
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-18 10:52:30 +0530

    Ability to extend employeepayroll

---
+++ UC8.sql
@@ -0,0 +1,63 @@
+create database EmployeeServices;
+
+--UC-2 creating Table
+create Table empoyee_payrolls
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+--UC-3 Insert values in Table
+Insert into empoyee_payroll(name,salary,startDate) values
+('Pavan',20000,'2021-03-12'),
+('Kalyan',25000,'2021-04-18'),
+('Yadav',10000,'2020-05-13'),
+('Madhu',30000,'2020-08-19');
+
+--UC-4 Retrieve All data--
+select * from empoyee_payroll;
+
+------- UC 5: Select Query using Cast() an GetDate() -------
+select salary from empoyee_payroll where name='Pavan';
+select salary from empoyee_payroll where startDate BETWEEN Cast('2020-12-20' as Date) and GetDate();
+
+------- UC 6: Add Gender Column and Update Table Values -------
+Alter table empoyee_payroll
+add Gender char(1);
+
+Update empoyee_payroll 
+set Gender='M'
+where name='Pavan';
+Update empoyee_payroll 
+set Gender='M'
+where name='Kalyan' or name='Yadav'or name='Madhu';
+
+------- UC 7: Use Aggregate Functions and Group by Gender -------
+
+select Sum(salary) as "TotalSalary",Gender from empoyee_payroll group by Gender;
+select Avg(salary) as "AverageSalary",Gender from empoyee_payroll group by Gender;
+select Min(salary) as "MinimumSalary",Gender from empoyee_payroll group by Gender;
+select Max(salary) as "MaximumSalary",Gender from empoyee_payroll group by Gender;
+select count(salary) as "CountSalary",Gender from empoyee_payroll group by Gender;
+
+
+------- UC 8: Add column department,PhoneNumber and Address -------
+Alter table empoyee_payroll
+add EmployeePhoneNumber BigInt,EmployeeDepartment varchar(200) not null default 'Publish',Address varchar(200) default 'Not Provided';
+
+Update empoyee_payroll 
+set EmployeePhoneNumber='1234567890',EmployeeDepartment='Civil',Address='Hyderabad,Telangana'
+where name='Pavan';
+
+Update empoyee_payroll 
+set EmployeePhoneNumber='10987252525',EmployeeDepartment='Mechanical',Address='Arizona,US'
+where name ='Kalyan';
+
+Update empoyee_payroll 
+set EmployeePhoneNumber='2134567890',EmployeeDepartment='IT',Address='Chennai,TN'
+where name ='Yadav';
+
+Update empoyee_payroll 
+set EmployeePhoneNumber='3214567890',EmployeeDepartment='Irrigation',Address='Bareilly,UP'
+where name ='Madhu';
